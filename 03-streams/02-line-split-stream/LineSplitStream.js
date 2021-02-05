32aecd97cc7b04b4b40bd0b12f323ac24e510f6d
Gushchina Mariya <Maria.GUSCHINA@raiffeisen.ru>
2021-02-05 14:11:47 +0300

    feat: add solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,36 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._prevLinePart = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = chunk.toString().split(os.EOL);
+    const linesCount = lines.length;
+    let linesQueue = [];
+
+    if (!linesCount) {
+      this._prevLinePart += chunk.toString();
+    } else if (lines[linesCount - 1]) {
+      if (this._prevLinePart) {
+        lines[0] = `${this._prevLinePart}${lines[0]}`;
+      }
+
+      linesQueue = linesQueue.concat(lines.slice(0, linesCount - 1));
+      for (const line of linesQueue) {
+        this.push(line);
+      }
+
+      this._prevLinePart = lines[linesCount - 1];
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    if (this._prevLinePart) {
+      callback(null, this._prevLinePart);
+    }
   }
 }
 
