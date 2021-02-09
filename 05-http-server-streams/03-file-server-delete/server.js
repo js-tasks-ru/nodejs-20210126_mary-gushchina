0a64c8300fa2cad338a312a45b892f1309faac56
Gushchina Mariya <Maria.GUSCHINA@raiffeisen.ru>
2021-02-09 23:33:32 +0300

    feat: add solution for 05-http-server-streams" "03-file-server-delete

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,10 +11,40 @@ server.on('request', (req, res) => {
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'DELETE':
+    case 'DELETE': {
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        res.end('Incorrect filename');
+        break;
+      }
+
+      if (!fs.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end('File is not found');
+        break;
+      }
+
+      const handleInternalServerError = () => {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      };
+
+      req.on('error', handleInternalServerError);
+
+      req.on('aborted', handleInternalServerError);
+
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        } else {
+          res.statusCode = 200;
+          res.end('File has been deleted successfully');
+        }
+      });
 
       break;
-
+    }
     default:
       res.statusCode = 501;
       res.end('Not implemented');
